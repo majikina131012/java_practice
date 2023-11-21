3633409261258cef523bcee1b793a607241cfa28
majikina131012 <majikina.ryuma.infratop@gmail.com>
2023-11-21 15:26:33 +0000

    ２章

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert('Hello World');
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while (num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+let i;
+let num = 0;
+
+for(i = 1; i < 101; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です')
